8ccd7e0eecc9c5c29ec79856bb924dab4dfaa1be
Jagg0611 <44612738+Jagg0611@users.noreply.github.com>
2026-09-21 02:15:20 +0200

    Publicar sincronizacion Supabase

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'control-gastos-v1';
+const CACHE_NAME = 'control-gastos-v2';
 const APP_FILES = ['index.html', 'control-gastos.webmanifest'];
 
 self.addEventListener('install', event => {
