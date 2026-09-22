948981b736105bb54f5c89df6768e6b928a57e55
Jagg0611 <44612738+Jagg0611@users.noreply.github.com>
2026-09-22 18:50:39 +0200

    Mejorar exportación a Excel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'control-gastos-v4';
+const CACHE_NAME = 'control-gastos-v5';
 const APP_FILES = ['index.html', 'control-gastos.webmanifest'];
 
 self.addEventListener('install', event => {
